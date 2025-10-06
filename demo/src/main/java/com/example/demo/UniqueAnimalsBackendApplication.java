b8e9a57a58f8dabb4a055fa8b8040001be001bdd
Daniii74 <D_asif@uncg.edu>
2025-10-06 12:34:36 -0400

    springboot commit

---
+++ demo/src/main/java/com/example/demo/UniqueAnimalsBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UniqueAnimalsBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UniqueAnimalsBackendApplication.class, args);
+	}
+
+}
